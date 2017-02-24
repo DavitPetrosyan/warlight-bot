096cf43a8869de37fca89b82a290107c016079a7
Hayk Movsisyan <hayk.movsisyan@sis2n061.local>
2017-02-24 21:18:27 +0400

    Initial commit

---
+++ src/com/synisis/ai/MyBot.java
@@ -0,0 +1,17 @@
+package com.synisis.ai;
+
+import com.synisis.ai.bot.BotParser;
+import com.synisis.ai.bot.BotStarter;
+
+import java.util.Scanner;
+
+public class MyBot {
+
+    public static void main(String[] args)
+    {
+        new BotStarter().start(args);
+
+        int a = 8;
+    }
+
+}
---
+++ src/com/synisis/ai/bot/Bot.java
@@ -0,0 +1,28 @@
+/**
+ * Warlight AI Game Bot
+ *
+ * Last update: April 02, 2014
+ *
+ * @author Jim van Eeden
+ * @version 1.0
+ * @License MIT License (http://opensource.org/Licenses/MIT)
+ */
+
+package com.synisis.ai.bot;
+
+import com.synisis.ai.main.Region;
+import com.synisis.ai.move.AttackTransferMove;
+import com.synisis.ai.move.PlaceArmiesMove;
+
+import java.util.ArrayList;
+
+
+public interface Bot {
+	
+	public ArrayList<Region> getPreferredStartingRegions(BotState state, Long timeOut);
+	
+	public ArrayList<PlaceArmiesMove> getPlaceArmiesMoves(BotState state, Long timeOut);
+	
+	public ArrayList<AttackTransferMove> getAttackTransferMoves(BotState state, Long timeOut);
+
+}
